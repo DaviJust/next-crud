da1b97fe65149fe8f93a805c5976f6c2ce3e40a5
Davi <davijustinocouto@gmail.com>
2023-01-10 11:50:41 -0300

    Criando e estilizando tabela de usuarios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,36 @@
-import Layout from './components/Layout'
+import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente('Bia', 45, "2"),
+    new Cliente('Carlos', 30, "3"),
+    new Cliente('Barbara', 50, "4"),
+    new Cliente('Ariel', 18, "5"),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log("Excluir..." + cliente.nome)
+  }
   return (
-    <div className='
+    <span className=' 
     flex justify-center items-center h-screen 
     bg-gradient-to-r from-black to-purple-800
     text-white' >
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
-        </Layout> 
-         </div>
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
+      </Layout>
+    </span>
   )
 }
